463db8b357cec9e7cfd459c54fbb53363d4e873b
ruakes <ruairi.kester@yahoo.co.uk>
2024-11-06 14:47:29 +0000

    individual article page viewable without comments section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 import Header from "../components/Header"
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../components/Homepage";
-import ArticlesList from "../components/ArticlesList"
-import ArticleCard from "../components/ArticleCard";
+import ArticlesList from "../components/ArticlesList";
+import IndividualArticle from "../components/IndividualArticle";
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage />}/>
         <Route path='/articles' element={<ArticlesList />}/>
-        <Route path='/articles/:article_id' element={<ArticleCard />}/>
+        <Route path='/articles/:article_id' element={<IndividualArticle />}/>
       </Routes>
     </>
   )
